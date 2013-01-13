184fe1df07790232bdc6eaf93e9c0e8b39e57e0b
Tommy Alex <iptux7@gmail.com>
2013-01-13 20:50:44 +0800

    DroidUi: add install script

---
+++ install.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM Copyright 2012-2013 by Tommy Alex. All Rights Reserved.
+REM
+REM Permission to use, copy, modify, and distribute this software and its
+REM documentation for any purpose and without fee is hereby granted,
+REM provided that the above copyright notice appear in all copies and that
+REM both that copyright notice and this permission notice appear in
+REM supporting documentation, and that the name of Vinay Sajip
+REM not be used in advertising or publicity pertaining to distribution
+REM of the software without specific, written prior permission.
+REM VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+REM ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+REM VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+REM ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+REM IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+REM OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+REM install.bat
+REM install DroidUi in your Android device
+
+set sl4a=/sdcard/sl4a/scripts/
+
+echo adb push DroidUi "%sl4a%DroidUi/"
+echo adb push sample "%sl4a%"
---
+++ install.sh
@@ -0,0 +1,23 @@
+# Copyright 2012-2013 by Tommy Alex. All Rights Reserved.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appear in all copies and that
+# both that copyright notice and this permission notice appear in
+# supporting documentation, and that the name of Vinay Sajip
+# not be used in advertising or publicity pertaining to distribution
+# of the software without specific, written prior permission.
+# VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+# ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+# ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# install.sh
+# install DroidUi on your Android device
+
+sl4a=/sdcard/sl4a/scripts/
+
+echo adb push DroidUi "${sl4a}DroidUi/"
+echo adb push sample "${sl4a}"
