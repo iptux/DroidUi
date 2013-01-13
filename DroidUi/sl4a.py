9ef8d72f9e0e3fe9d826f0b2257f7107fdfbeef4
Tommy Alex <iptux7@gmail.com>
2013-01-13 19:44:26 +0800

    DroidUi: make DroidUi a library

---
+++ DroidUi/sl4a.py
@@ -0,0 +1,38 @@
+# Copyright 2012-2013 by Tommy Alex. All Rights Reserved.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appear in all copies and that
+# both that copyright notice and this permission notice appear in
+# supporting documentation, and that the name of Vinay Sajip
+# not be used in advertising or publicity pertaining to distribution
+# of the software without specific, written prior permission.
+# VINAY SAJIP DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+# ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+# ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# sl4a.py
+# wrapper for android.Android()
+#
+# Author: Alex.Wang
+# Create: 2012-02-05 23:36
+
+
+from android import *
+
+
+class sl4aError(Exception):
+	pass
+
+
+class sl4a(Android):
+	def __getattr__(self, name):
+		def rpc_call(*args):
+			r = self._rpc(name, *args)
+			if r.error:
+				raise sl4aError, r.error
+			return r.result
+		return rpc_call
