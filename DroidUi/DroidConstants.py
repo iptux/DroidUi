361cd78347a43c3a1b6e6008874f3cfc4e058c28
Tommy Alex <iptux7@gmail.com>
2013-05-21 00:52:33 +0800

    DroidConstants: move attribrute used by DroidUi to the head of file

--- DroidUi/DroidConstants.py
+++ DroidUi/DroidConstants.py
@@ -16,9 +16,94 @@
 
 # Symbolic constants for DroidUi
 
+#########
+# DroidUi
+
+def joinattr(*attrs):
+	'''join many attribute together'''
+	return '|'.join(attrs)
+
 # boolean
 TRUE = 'true'
 FALSE = 'false'
+
+# layout_width and layout_height
+WRAP_CONTENT = 'wrap_content'
+MATCH_PARENT = 'match_parent'
+FILL_PARENT = 'fill_parent'
+
+# gravity and layout_gravity
+TOP = 'top'
+BOTTOM = 'bottom'
+LEFT = 'left'
+RIGHT = 'right'
+CENTER = 'center'
+FILL = 'FILL'
+CENTER_VERTICAL = 'center_vertical'
+CENTER_HORIZONTAL = 'center_horizontal'
+FILL_VERTICAL = 'fill_vertical'
+FILL_HORIZONTAL = 'fill_horizontal'
+CLIP_VERTICAL = 'clip_vertical'
+CLIP_HORIZONTAL = 'clip_horizontal'
+
+# misc
+NONE = 'none'
+HORIZONTAL = 'horizontal'
+VERTICAL = 'vertical'
+
+# visibility
+VISIBLE = 'visible'
+INVISIBLE = 'invisible'
+GONE = 'gone'
+
+# scrollbarStyle
+INSIDE_OVERLAY = 'insideOverlay'
+INSIDE_INSET = 'insideInset'
+OUTSIDE_OVERLAY = 'outsideOverlay'
+OUTSIDE_INSET = 'outsideInset'
+
+# textStyle
+NORMAL = 'normal'
+BOLD = 'bold'
+ITALIC = 'italic'
+
+# typeface
+SANS = 'sans'
+SERIF = 'serif'
+MONOSPACE = 'monospace'
+
+# inputType
+TEXT = 'text'
+DATE = 'date'
+TIME = 'time'
+DATETIME = 'datetime'
+PHONE = 'phone'
+NUMBER = 'number'
+NUMBER_SIGNED = 'numberSigned'
+NUMBER_DECIMAL = 'numberDecimal'
+TEXT_CAP_CHARACTERS = 'textCapCharacters'
+TEXT_CAP_WORDS = 'textCapWords'
+TEXT_CAP_SENTENCES = 'textCapSentences'
+TEXT_AUTO_CORRECT = 'textAutoCorrect'
+TEXT_AUTO_COMPLETE = 'textAutoComplete'
+TEXT_MULTI_LINE = 'textMultiLine'
+TEXT_IME_MULTI_LINE = 'textImeMultiLine'
+TEXT_NO_SUGGESTIONS = 'textNoSuggestions'
+TEXT_URI = 'textUri'
+TEXT_EMAIL_ADDRESS = 'textEmailAddress'
+TEXT_EMAIL_SUBJECT = 'textEmailSubject'
+TEXT_SHORT_MESSAGE = 'textShortMessage'
+TEXT_LONG_MESSAGE = 'textLongMessage'
+TEXT_PERSON_NAME = 'textPersonName'
+TEXT_POSTAL_ADDRESS = 'textPostalAddress'
+TEXT_PASSWORD = 'textPassword'
+TEXT_VISIBLE_PASSWORD = 'textVisiblePassword'
+TEXT_WEB_EDIT_TEXT = 'textWebEditText'
+TEXT_FILTER = 'textFilter'
+TEXT_PHONETIC = 'textPhonetic'
+
+#############
+# DroidFacade
 
 # SmsFacade
 INBOX = 'inbox'
@@ -42,6 +127,8 @@ Z = 4
 XZ = 5
 YZ = 6
 XYZ = 7
+
+#####
 
 # key
 KEY_UNKNOWN = 0
@@ -264,83 +351,3 @@ KEY_YEN = 216
 KEY_RO = 217
 KEY_KANA = 218
 KEY_ASSIST = 219
-
-# layout_width and layout_height
-WRAP_CONTENT = 'wrap_content'
-MATCH_PARENT = 'match_parent'
-FILL_PARENT = 'fill_parent'
-
-# gravity and layout_gravity
-TOP = 'top'
-BOTTOM = 'bottom'
-LEFT = 'left'
-RIGHT = 'right'
-CENTER = 'center'
-FILL = 'FILL'
-CENTER_VERTICAL = 'center_vertical'
-CENTER_HORIZONTAL = 'center_horizontal'
-FILL_VERTICAL = 'fill_vertical'
-FILL_HORIZONTAL = 'fill_horizontal'
-CLIP_VERTICAL = 'clip_vertical'
-CLIP_HORIZONTAL = 'clip_horizontal'
-
-# inputType
-TEXT = 'text'
-DATE = 'date'
-TIME = 'time'
-DATETIME = 'datetime'
-PHONE = 'phone'
-NUMBER = 'number'
-NUMBER_SIGNED = 'numberSigned'
-NUMBER_DECIMAL = 'numberDecimal'
-TEXT_CAP_CHARACTERS = 'textCapCharacters'
-TEXT_CAP_WORDS = 'textCapWords'
-TEXT_CAP_SENTENCES = 'textCapSentences'
-TEXT_AUTO_CORRECT = 'textAutoCorrect'
-TEXT_AUTO_COMPLETE = 'textAutoComplete'
-TEXT_MULTI_LINE = 'textMultiLine'
-TEXT_IME_MULTI_LINE = 'textImeMultiLine'
-TEXT_NO_SUGGESTIONS = 'textNoSuggestions'
-TEXT_URI = 'textUri'
-TEXT_EMAIL_ADDRESS = 'textEmailAddress'
-TEXT_EMAIL_SUBJECT = 'textEmailSubject'
-TEXT_SHORT_MESSAGE = 'textShortMessage'
-TEXT_LONG_MESSAGE = 'textLongMessage'
-TEXT_PERSON_NAME = 'textPersonName'
-TEXT_POSTAL_ADDRESS = 'textPostalAddress'
-TEXT_PASSWORD = 'textPassword'
-TEXT_VISIBLE_PASSWORD = 'textVisiblePassword'
-TEXT_WEB_EDIT_TEXT = 'textWebEditText'
-TEXT_FILTER = 'textFilter'
-TEXT_PHONETIC = 'textPhonetic'
-
-# scrollbarStyle
-INSIDE_OVERLAY = 'insideOverlay'
-INSIDE_INSET = 'insideInset'
-OUTSIDE_OVERLAY = 'outsideOverlay'
-OUTSIDE_INSET = 'outsideInset'
-
-# textStyle
-NORMAL = 'normal'
-BOLD = 'bold'
-ITALIC = 'italic'
-
-# typeface
-SANS = 'sans'
-SERIF = 'serif'
-MONOSPACE = 'monospace'
-
-# visibility
-VISIBLE = 'visible'
-INVISIBLE = 'invisible'
-GONE = 'gone'
-
-# misc
-NONE = 'none'
-HORIZONTAL = 'horizontal'
-VERTICAL = 'vertical'
-
-
-def joinattr(*attrs):
-	'''join many attribute together'''
-	return '|'.join(attrs)
