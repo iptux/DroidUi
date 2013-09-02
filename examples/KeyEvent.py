852ee8836a2218394a09e8774fb4766fd495ca13
Tommy Alex <iptux7@gmail.com>
2013-09-03 00:17:19 +0800

    update KeyEvent

--- examples/KeyEvent.py
+++ examples/KeyEvent.py
@@ -7,7 +7,29 @@
 # Create: 2012-11-04 23:33
 
 
-import DroidUi
+import DroidUi as Ui
 
-DroidUi.DroidUi().mainloop()
+
+def display_event(data):
+	text = 'Key %s is pressed' % data['key']
+	view.config(text = text)
+	return True
+
+
+def main():
+	global view
+	gui = Ui.DroidUi()
+	view = Ui.TextView(gui,
+		text = 'Press any key',
+		textSize = '40sp',
+		layout_width = Ui.FILL_PARENT,
+		gravity = Ui.CENTER,
+	)
+	Ui.Button(gui, text = 'Quit', command = gui.quit)
+	gui.reg_event('key', display_event)
+	gui.mainloop()
+
+
+if __name__ == '__main__':
+	main()
 
